9e0502634391c11487aa902a8b947e7d717c2493
maks5345 <maksliashchuk@gmail.com>
2024-05-15 21:02:17 +0300

    321

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Lab2',
+  tagline: 'Лабораторна робота #2',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
